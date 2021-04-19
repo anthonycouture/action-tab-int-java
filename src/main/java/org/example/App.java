b607351b2a0201d873d7b6e5b65c183c4e7b1116
Anthony Couture <antobox28@gmail.com>
2021-04-19 13:16:13 +0200

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class App {
+
+    public static void procheZero(int[] tabInt) {
+        AtomicReference<OptionalInt> result = new AtomicReference<>(OptionalInt.empty());
+        AtomicReference<OptionalInt> ecart = new AtomicReference<>(OptionalInt.empty());
+        Arrays.stream(tabInt)
+                .distinct()
+                .forEach(value -> {
+                    int ecartValue = value < 0 ? 0 - value : value;
+                    if (ecart.get().isEmpty() || ecart.get().getAsInt() > ecartValue || (result.get().getAsInt() < value && (ecart.get().getAsInt() == ecartValue))) {
+                        result.set(OptionalInt.of(value));
+                        ecart.set(OptionalInt.of(ecartValue));
+                    }
+                });
+        if (result.get().isPresent()) {
+            System.out.println("Le chiffre le plus proche de 0 est " + result.get().getAsInt());
+        } else {
+            System.out.println("il n'y a pas de chiffre");
+        }
+    }
+
+    public static void plusPetit(int[] tab){
+        OptionalInt result = Arrays.stream(tab)
+                .min();
+        if(result.isPresent()){
+            System.out.println("Le plus petit est "+ result.getAsInt());
+        } else {
+            System.out.println("Pas de nombre dans le tableau");
+        }
+    }
+
+    public static void plusGrand(int[] tab){
+        OptionalInt result = Arrays.stream(tab)
+                .max();
+        if(result.isPresent()){
+            System.out.println("Le plus grand est "+ result.getAsInt());
+        } else {
+            System.out.println("Pas de nombre dans le tableau");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int[] tab = {2, 1, 3, -2, -1, 3, 1};
+        procheZero(tab);
+        plusPetit(tab);
+        plusGrand(tab);
+    }
+}
